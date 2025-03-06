2916880fe77fd3620502eedb280ef8c6f58f5f9d
watteja <mateyast@gmail.com>
2025-03-06 15:38:12 +0100

    Extracted Todo component

--- todo-app/todo-frontend/src/Todos/List.jsx
+++ todo-app/todo-frontend/src/Todos/List.jsx
@@ -1,52 +1,18 @@
 import PropTypes from "prop-types";
+import Todo from "./Todo";
 
 const TodoList = ({ todos, deleteTodo, completeTodo }) => {
-  const onClickDelete = (todo) => () => {
-    deleteTodo(todo);
-  };
-
-  const onClickComplete = (todo) => () => {
-    completeTodo(todo);
-  };
-
   return (
     <>
       {todos
-        .map((todo) => {
-          const doneInfo = (
-            <>
-              <span>This todo is done</span>
-              <span>
-                <button onClick={onClickDelete(todo)}> Delete </button>
-              </span>
-            </>
-          );
-
-          const notDoneInfo = (
-            <>
-              <span>This todo is not done</span>
-              <span>
-                <button onClick={onClickDelete(todo)}> Delete </button>
-                <button onClick={onClickComplete(todo)}> Set as done </button>
-              </span>
-            </>
-          );
-
-          return (
-            <div
-              key={todo._id}
-              style={{
-                display: "flex",
-                justifyContent: "space-between",
-                maxWidth: "70%",
-                margin: "auto",
-              }}
-            >
-              <span>{todo.text}</span>
-              {todo.done ? doneInfo : notDoneInfo}
-            </div>
-          );
-        })
+        .map((todo) => (
+          <Todo
+            key={todo._id}
+            todo={todo}
+            deleteTodo={deleteTodo}
+            completeTodo={completeTodo}
+          />
+        ))
         .reduce((acc, cur, idx) => [...acc, <hr key={`hr-${idx}`} />, cur], [])}
     </>
   );
